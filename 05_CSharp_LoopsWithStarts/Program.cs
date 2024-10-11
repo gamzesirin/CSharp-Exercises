eb7f0f479fb413b91a0818054a1979974307a42e
Gamze Şirin <gamze1sirin@gmail.com>
2024-10-11 18:33:33 +0300

    feat: added LoopsWithStarts

---
+++ 05_CSharp_LoopsWithStarts/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_CSharp_LoopsWithStarts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region alt alta 10 tane yıldız yapma
+
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        Console.WriteLine("*");
+            //    }
+
+            //Console.Read();
+            #endregion
+
+            #region yan yana 10 tane yıldız yapma
+
+            //for (int j = 0; j < 10; j++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            //Console.Read();
+            #endregion
+
+
+            #region alt alata 10 satır ve her satırda 10 tane yıldız yapma
+
+            //for (int j = 0; j < 10; j++)
+            //{
+            //    for (int k = 0; k < 10; k++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            //Console.Read();
+            #endregion
+
+            #region dik üçgen şeklinde yıldız yapma
+
+            //for (int j = 0; j < 10; j++)
+            //{
+            //    for (int k = 0; k < j; k++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            //Console.Read();
+            #endregion
+
+            #region ters dik üçgen şeklinde yıldız yapma
+
+            //for (int j = 10; j > 0; j--)
+            //{
+            //    for (int k = 0; k <j; k++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            //Console.Read();
+            #endregion
+
+            #region  dik ve ters düi üçgen birlikte olacak şeklinde yıldız yapma
+
+            //for (int j = 0; j < 10; j++)
+            //{
+
+            //    for (int k = 0; k <=j; k++)
+            //    {
+
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int m = 9; m >= 0; m--)
+            //{
+
+            //    for (int i = 0; i <= m; i++)
+            //    {
+
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.Read();
+            #endregion
+
+            #region gruplu  şekilde yıldız yapma
+            //int n = 5;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 0; k <= i; k++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //}
+            //Console.Read();
+            #endregion
+
+
+
+        }
+    }
+}
